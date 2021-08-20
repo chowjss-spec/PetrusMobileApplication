b2009fdfbe88cf820e6f03bc0fbf9def85af2788
Shirley <chowjss@gmail.com>
2021-08-20 10:04:50 +0800

    Contact seller button added

--- app/src/main/java/com/example/petrusapplication/ProductDetailActivity.java
+++ app/src/main/java/com/example/petrusapplication/ProductDetailActivity.java
@@ -6,7 +6,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.petrusapplication.db.Product;
@@ -25,5 +29,18 @@ public class ProductDetailActivity extends AppCompatActivity {
         product= (Product) getIntent().getSerializableExtra("product");
         TextView content=findViewById(R.id.productDetails);
         content.setText("Name:\n"+product.getName()+"\n\nDescription:\n"+product.getDescription()+"\n\nQuantity:\n"+product.getQuantity()+"\n\nContact:\n"+product.getEmail());
+        Button contactButton=findViewById(R.id.contactButton);
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("mailto:"+product.getEmail());
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "For Enquiry");
+                intent.putExtra(Intent.EXTRA_TEXT, "Please state your enquiry");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
